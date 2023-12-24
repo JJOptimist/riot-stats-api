f9d1e8e516f5665ea4bbb93152265eaec18e0b39
JJOptimist <86833563+JJOptimist@users.noreply.github.com>
2023-12-24 22:02:19 +0100

    fixed proxy and working matches

--- riotapi/src/App.js
+++ riotapi/src/App.js
@@ -1,12 +1,9 @@
+// App.js
 import React, { useState } from 'react';
 import axiosInstance from './axiosInstance';
 import SummonerForm from './SummonerForm';
 import MatchList from './MatchList';
 import MatchDetails from './MatchDetails';
-
-const API_KEY = 'RGAPI-4278be8f-4e24-4ea3-bc98-2528230da0f5'; 
-const API_BASE_URL = 'https://eun1.api.riotgames.com/lol';
-
 
 function App() {
   const [summoner, setSummoner] = useState(null);
@@ -16,21 +13,13 @@ function App() {
   const searchSummoner = async (summonerName) => {
     try {
       // Step 1: Retrieve Summoner Information
-      const summonerResponse = await axiosInstance.get(
-        `/summoner/v4/summoners/by-name/${summonerName}?api_key=${API_KEY}`
-        
-      );
-      console.log("Probao ovde");
-
+      const summonerResponse = await axiosInstance.get(`/summoner/v4/summoners/by-name/${summonerName}`);
       const summonerData = summonerResponse.data;
       console.log(summonerData);
       setSummoner(summonerData);
 
       // Step 2: Retrieve Match IDs
-      const matchIdsResponse = await axiosInstance.get(
-        `/match/v5/matches/by-puuid/${summonerData.puuid}/ids?api_key=${API_KEY}&count=10`
-      );
-
+      const matchIdsResponse = await axiosInstance.get(`/match/v5/matches/by-puuid/${summonerData.puuid}/ids`);
       const matchIdsData = matchIdsResponse.data;
       setMatches(matchIdsData);
     } catch (error) {
@@ -39,12 +28,8 @@ function App() {
   };
 
   const selectMatch = async (matchId) => {
-    // Fetch additional details for the selected match
     try {
-      const matchDetailsResponse = await axiosInstance.get(
-        `/match/v5/matches/${matchId}?api_key=${API_KEY}`
-      );
-
+      const matchDetailsResponse = await axiosInstance.get(`/match/v5/matches/${matchId}`);
       const matchDetailsData = matchDetailsResponse.data;
       setSelectedMatch(matchDetailsData);
     } catch (error) {
--- riotapi/src/axiosInstance.js
+++ riotapi/src/axiosInstance.js
@@ -1,11 +1,22 @@
 // axiosInstance.js
-
 import axios from 'axios';
 
-const API_BASE_URL = 'https://eun1.api.riotgames.com/lol'; 
-
 const instance = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: 'https://eun1.api.riotgames.com/lol', // Default base URL, adjust as needed
+});
+
+instance.interceptors.request.use((config) => {
+  const API_KEY = 'RGAPI-4278be8f-4e24-4ea3-bc98-2528230da0f5'; // Replace with your actual API key
+  config.params = config.params || {};
+  config.params['api_key'] = API_KEY;
+  config.params['count'] = 10; // Set the default count parameter
+
+  // Adjust base URL for specific endpoints
+  if (config.url.includes('/match/v5/matches/by-puuid')) {
+    config.baseURL = 'https://europe.api.riotgames.com/lol'; // Adjust the base URL for this endpoint
+  }
+
+  return config;
 });
 
 export default instance;
