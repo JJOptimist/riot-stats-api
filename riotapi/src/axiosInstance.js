b3bd7f68bb4009f1f46caff53a8e1b8ab30f1f7d
JJOptimist <86833563+JJOptimist@users.noreply.github.com>
2023-12-24 22:09:29 +0100

    fixed routing for europe

--- riotapi/src/axiosInstance.js
+++ riotapi/src/axiosInstance.js
@@ -2,18 +2,18 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://eun1.api.riotgames.com/lol', // Default base URL, adjust as needed
+  baseURL: 'https://eun1.api.riotgames.com/lol', 
 });
 
 instance.interceptors.request.use((config) => {
-  const API_KEY = 'RGAPI-4278be8f-4e24-4ea3-bc98-2528230da0f5'; // Replace with your actual API key
+  const API_KEY = 'RGAPI-4278be8f-4e24-4ea3-bc98-2528230da0f5'; 
   config.params = config.params || {};
   config.params['api_key'] = API_KEY;
-  config.params['count'] = 10; // Set the default count parameter
+  config.params['count'] = 10; 
 
-  // Adjust base URL for specific endpoints
-  if (config.url.includes('/match/v5/matches/by-puuid')) {
-    config.baseURL = 'https://europe.api.riotgames.com/lol'; // Adjust the base URL for this endpoint
+
+  if (config.url.includes('/match/v5/matches')) {
+    config.baseURL = 'https://europe.api.riotgames.com/lol'; 
   }
 
   return config;
