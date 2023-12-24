70002b37efe0e0ab070067c4ad7e2e2b973ac471
JJOptimist <86833563+JJOptimist@users.noreply.github.com>
2023-12-24 22:45:08 +0100

    first wave for `css` added

--- riotapi/src/App.js
+++ riotapi/src/App.js
@@ -10,6 +10,13 @@ function App() {
   const [matches, setMatches] = useState([]);
   const [selectedMatch, setSelectedMatch] = useState(null);
 
+  const [stats, setStats] = useState({
+    kills: null,
+    deaths: null,
+    assists: null,
+    goldEarned: null,
+  });
+
   const searchSummoner = async (summonerName) => {
     try {
       // Step 1: Retrieve Summoner Information
@@ -29,20 +36,59 @@ function App() {
 
   const selectMatch = async (matchId) => {
     try {
-      const matchDetailsResponse = await axiosInstance.get(`/match/v5/matches/${matchId}`);
+      const matchDetailsResponse = await axiosInstance.get(
+        `/match/v5/matches/${matchId}`
+      );
+
       const matchDetailsData = matchDetailsResponse.data;
       setSelectedMatch(matchDetailsData);
+
+      // Check if summoner is not null
+      if (summoner) {
+        // Find the participant corresponding to the summoner
+        const participant = matchDetailsData.info.participants.find(
+          (p) => p.puuid === summoner.puuid
+        );
+
+        if (participant) {
+          // Extract kills, deaths, assists, and goldEarned
+          const kills = participant.kills;
+          const deaths = participant.deaths;
+          const assists = participant.assists;
+          const goldEarned = participant.goldEarned;
+
+          // Update the state to trigger a re-render
+          setStats({
+            kills: kills,
+            deaths: deaths,
+            assists: assists,
+            goldEarned: goldEarned,
+          });
+        }
+      }
     } catch (error) {
       console.error('Error fetching match details:', error);
     }
   };
+  
 
   return (
-    <div>
+    <div className='content'>
+      <div className='stats'>
       <SummonerForm onSearch={searchSummoner} />
       {summoner && <h1>{summoner.name}</h1>}
       <MatchList matches={matches} onSelectMatch={selectMatch} />
       {selectedMatch && <MatchDetails details={selectedMatch} />}
+      
+
+      {/* Display stats */}
+      <div>
+        <p>Kills: {stats.kills}</p>
+        <p>Deaths: {stats.deaths}</p>
+        <p>Assists: {stats.assists}</p>
+        <p>Gold Earned: {stats.goldEarned}</p>
+      </div>
+      </div>
     </div>
   );
 }
